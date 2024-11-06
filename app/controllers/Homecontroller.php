f7f287f413967bbbd9ece9e1752a26b84c01cd07
rpariona81 <ronald.pariona.isidro@gmail.com>
2024-11-05 22:49:01 -0500

    si esta logueado que lo redirija a su guard

--- app/controllers/Homecontroller.php
+++ app/controllers/Homecontroller.php
@@ -1,14 +1,20 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class HomeController extends CI_Controller {
+class HomeController extends CI_Controller
+{
 
     public function __construct()
     {
         parent::__construct();
     }
+
+
     public function index()
     {
+        if ($this->session->userdata('user_guard') != NULL) {
+            redirect(base_url() . $this->session->userdata('user_guard'));
+        }
         $this->load->view('home');
         //$this->load->view('hometest');
     }
@@ -28,7 +34,6 @@ class HomeController extends CI_Controller {
         $this->output->set_header("Cache-Control: no-store, no-cache, must-revalidate, no-transform, max-age=0, post-check=0, pre-check=0");
         $this->output->set_header("Pragma: no-cache");
     }
-
 }
 
 /* End of file Controllername.php */
