6412a138edc0f406cb687ff1196b4bb20af6f92a
rpariona81 <ronald.pariona.isidro@gmail.com>
2025-04-25 17:37:05 -0500

    logo en lateral en base_url user

--- app/views/app/templateApp.php
+++ app/views/app/templateApp.php
@@ -5,17 +5,16 @@
 <head>
     <meta charset="utf-8" />
     <title><?= getenv('APP_NAME') ?></title>
-    <meta name="description"
-        content="Servicios al estudiante." />
-    <meta name="keywords"
-        content="bolsa laboral, Biblioteca virtual, Portales web" />
+    <meta name="description" content="Servicios al estudiante." />
+    <meta name="keywords" content="bolsa laboral, Biblioteca virtual, Portales web" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="shortcut icon" href="<?= base_url() ?>assets/media/logos/favicon.ico" />
     <!--begin::Fonts-->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
     <!--end::Fonts-->
     <!--begin::Page Vendor Stylesheets(used by this page)-->
-    <link href="<?= base_url() ?>assets/plugins/custom/fullcalendar/fullcalendar.bundle.css" rel="stylesheet" type="text/css" />
+    <link href="<?= base_url() ?>assets/plugins/custom/fullcalendar/fullcalendar.bundle.css" rel="stylesheet"
+        type="text/css" />
     <!--end::Page Vendor Stylesheets-->
     <!--begin::Global Stylesheets Bundle(used by all pages)-->
     <link href="<?= base_url() ?>assets/plugins/global/plugins.bundle.css" rel="stylesheet" type="text/css" />
@@ -26,7 +25,8 @@
 <!--end::Head-->
 <!--begin::Body-->
 
-<body id="kt_body" class="header-fixed header-tablet-and-mobile-fixed toolbar-enabled toolbar-fixed" style="--kt-toolbar-height:55px;--kt-toolbar-height-tablet-and-mobile:55px">
+<body id="kt_body" class="header-fixed header-tablet-and-mobile-fixed toolbar-enabled toolbar-fixed"
+    style="--kt-toolbar-height:55px;--kt-toolbar-height-tablet-and-mobile:55px">
     <!--begin::Main-->
     <!--begin::Root-->
     <div class="d-flex flex-column flex-root">
@@ -44,7 +44,9 @@
                         <div class="d-flex align-items-center flex-grow-1 flex-lg-grow-0 me-lg-15">
                             <a href="<?= base_url('/' . $this->session->userdata('user_guard')) ?>">
                                 <!--<img alt="Logo" src="<?= base_url() ?>assets/media/logos/logo-1.svg" class="h-20px h-lg-30px" />-->
-                                <img alt="Logo" src="<?php echo (ENVIRONMENT === 'development') ?  'assets/media/logos/escudo.jpeg'  : getenv('APP_LOGO') ?>" class="h-20px h-lg-30px" />
+                                <img alt="Logo"
+                                    src="<?php echo (ENVIRONMENT === 'development') ?  'assets/media/logos/escudo.jpeg'  : base_url() . getenv('APP_LOGO') ?>"
+                                    class="h-20px h-lg-30px" />
                             </a>
                         </div>
                         <!--end::Logo-->
@@ -53,11 +55,19 @@
                             <!--begin::Navbar-->
                             <div class="d-flex align-items-stretch" id="kt_header_nav">
                                 <!--begin::Menu wrapper-->
-                                <div class="header-menu align-items-stretch" data-kt-drawer="true" data-kt-drawer-name="header-menu" data-kt-drawer-activate="{default: true, lg: false}" data-kt-drawer-overlay="true" data-kt-drawer-width="{default:'200px', '300px': '250px'}" data-kt-drawer-direction="end" data-kt-drawer-toggle="#kt_header_menu_mobile_toggle" data-kt-swapper="true" data-kt-swapper-mode="prepend" data-kt-swapper-parent="{default: '#kt_body', lg: '#kt_header_nav'}">
+                                <div class="header-menu align-items-stretch" data-kt-drawer="true"
+                                    data-kt-drawer-name="header-menu"
+                                    data-kt-drawer-activate="{default: true, lg: false}" data-kt-drawer-overlay="true"
+                                    data-kt-drawer-width="{default:'200px', '300px': '250px'}"
+                                    data-kt-drawer-direction="end" data-kt-drawer-toggle="#kt_header_menu_mobile_toggle"
+                                    data-kt-swapper="true" data-kt-swapper-mode="prepend"
+                                    data-kt-swapper-parent="{default: '#kt_body', lg: '#kt_header_nav'}">
                                     <!--begin::Menu-->
-                                    <div class="menu menu-lg-rounded menu-column menu-lg-row menu-state-bg menu-title-gray-700 menu-state-title-primary menu-state-icon-primary menu-state-bullet-primary menu-arrow-gray-400 fw-bold my-5 my-lg-0 align-items-stretch" id="#kt_header_menu" data-kt-menu="true">
+                                    <div class="menu menu-lg-rounded menu-column menu-lg-row menu-state-bg menu-title-gray-700 menu-state-title-primary menu-state-icon-primary menu-state-bullet-primary menu-arrow-gray-400 fw-bold my-5 my-lg-0 align-items-stretch"
+                                        id="#kt_header_menu" data-kt-menu="true">
                                         <div class="menu-item me-lg-1">
-                                            <a class="menu-link py-3" href="<?= base_url('/' . $this->session->userdata('user_guard') . '/') ?>">
+                                            <a class="menu-link py-3"
+                                                href="<?= base_url('/' . $this->session->userdata('user_guard') . '/') ?>">
                                                 <span class="menu-title">Catálogos</span>
                                             </a>
                                         </div>
@@ -69,7 +79,8 @@
                                         </div> -->
 
                                         <div class="menu-item me-lg-1">
-                                            <a class="menu-link py-3" href="<?= base_url('/' . $this->session->userdata('user_guard') . '/perfil') ?>">
+                                            <a class="menu-link py-3"
+                                                href="<?= base_url('/' . $this->session->userdata('user_guard') . '/perfil') ?>">
                                                 <span class="menu-title">Mi perfil</span>
                                             </a>
                                         </div>
@@ -89,29 +100,36 @@
                                     <div class="d-flex align-items-center ms-1 ms-lg-3" id="kt_header_user_menu_toggle">
                                         <!--begin::Menu wrapper-->
 
-                                        <div class="cursor-pointer symbol symbol-30px symbol-md-40px" data-kt-menu-trigger="click" data-kt-menu-attach="parent" data-kt-menu-placement="bottom-end">
+                                        <div class="cursor-pointer symbol symbol-30px symbol-md-40px"
+                                            data-kt-menu-trigger="click" data-kt-menu-attach="parent"
+                                            data-kt-menu-placement="bottom-end">
                                             <a href="#"
                                                 class="fw-bolder text-hover-primary fs-6"><?= $this->session->userdata('user_role_title') ?></a>
 
                                             <img src="<?= base_url() ?>assets/media/avatars/150-26.jpg" alt="user" />
                                         </div>
                                         <!--begin::Menu-->
-                                        <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg menu-state-primary fw-bold py-4 fs-6 w-275px" data-kt-menu="true">
+                                        <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg menu-state-primary fw-bold py-4 fs-6 w-275px"
+                                            data-kt-menu="true">
                                             <!--begin::Menu item-->
                                             <div class="menu-item px-3">
                                                 <div class="menu-content d-flex align-items-center px-3">
 
                                                     <!--begin::Avatar-->
                                                     <div class="symbol symbol-50px me-5">
-                                                        <img alt="Logo" src="<?= base_url() ?>assets/media/avatars/150-26.jpg" />
+                                                        <img alt="Logo"
+                                                            src="<?= base_url() ?>assets/media/avatars/150-26.jpg" />
                                                     </div>
                                                     <!--end::Avatar-->
                                                     <!--begin::Username-->
                                                     <div class="d-flex flex-column">
-                                                        <div class="fw-bolder d-flex align-items-center fs-5"><?= $this->session->userdata('user_login') ?>
+                                                        <div class="fw-bolder d-flex align-items-center fs-5">
+                                                            <?= $this->session->userdata('user_login') ?>
                                                         </div>
                                                         <div class="d-flex flex-column fs-6">
-                                                            <a href="<?= base_url('/' . $this->session->userdata('user_guard') . '/perfil') ?>" class="fw-bold text-muted text-hover-primary fs-7">Mi perfil</a>
+                                                            <a href="<?= base_url('/' . $this->session->userdata('user_guard') . '/perfil') ?>"
+                                                                class="fw-bold text-muted text-hover-primary fs-7">Mi
+                                                                perfil</a>
                                                         </div>
                                                         <!--end::Username-->
                                                     </div>
@@ -134,13 +152,20 @@
                                     </div>
                                     <!--end::User -->
                                     <!--begin::Heaeder menu toggle-->
-                                    <div class="d-flex align-items-center d-lg-none ms-2 me-n3" title="Show header menu">
-                                        <div class="btn btn-icon btn-active-light-primary w-30px h-30px w-md-40px h-md-40px" id="kt_header_menu_mobile_toggle">
+                                    <div class="d-flex align-items-center d-lg-none ms-2 me-n3"
+                                        title="Show header menu">
+                                        <div class="btn btn-icon btn-active-light-primary w-30px h-30px w-md-40px h-md-40px"
+                                            id="kt_header_menu_mobile_toggle">
                                             <!--begin::Svg Icon | path: icons/duotune/text/txt001.svg-->
                                             <span class="svg-icon svg-icon-1">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                                                    <path d="M13 11H3C2.4 11 2 10.6 2 10V9C2 8.4 2.4 8 3 8H13C13.6 8 14 8.4 14 9V10C14 10.6 13.6 11 13 11ZM22 5V4C22 3.4 21.6 3 21 3H3C2.4 3 2 3.4 2 4V5C2 5.6 2.4 6 3 6H21C21.6 6 22 5.6 22 5Z" fill="black" />
-                                                    <path opacity="0.3" d="M21 16H3C2.4 16 2 15.6 2 15V14C2 13.4 2.4 13 3 13H21C21.6 13 22 13.4 22 14V15C22 15.6 21.6 16 21 16ZM14 20V19C14 18.4 13.6 18 13 18H3C2.4 18 2 18.4 2 19V20C2 20.6 2.4 21 3 21H13C13.6 21 14 20.6 14 20Z" fill="black" />
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                                    viewBox="0 0 24 24" fill="none">
+                                                    <path
+                                                        d="M13 11H3C2.4 11 2 10.6 2 10V9C2 8.4 2.4 8 3 8H13C13.6 8 14 8.4 14 9V10C14 10.6 13.6 11 13 11ZM22 5V4C22 3.4 21.6 3 21 3H3C2.4 3 2 3.4 2 4V5C2 5.6 2.4 6 3 6H21C21.6 6 22 5.6 22 5Z"
+                                                        fill="black" />
+                                                    <path opacity="0.3"
+                                                        d="M21 16H3C2.4 16 2 15.6 2 15V14C2 13.4 2.4 13 3 13H21C21.6 13 22 13.4 22 14V15C22 15.6 21.6 16 21 16ZM14 20V19C14 18.4 13.6 18 13 18H3C2.4 18 2 18.4 2 19V20C2 20.6 2.4 21 3 21H13C13.6 21 14 20.6 14 20Z"
+                                                        fill="black" />
                                                 </svg>
                                             </span>
                                             <!--end::Svg Icon-->
@@ -164,9 +189,12 @@
                         <!--begin::Container-->
                         <div id="kt_toolbar_container" class="container-xxl d-flex flex-stack">
                             <!--begin::Page title-->
-                            <div data-kt-swapper="true" data-kt-swapper-mode="prepend" data-kt-swapper-parent="{default: '#kt_content_container', 'lg': '#kt_toolbar_container'}" class="page-title d-flex align-items-center flex-wrap me-3 mb-5 mb-lg-0">
+                            <div data-kt-swapper="true" data-kt-swapper-mode="prepend"
+                                data-kt-swapper-parent="{default: '#kt_content_container', 'lg': '#kt_toolbar_container'}"
+                                class="page-title d-flex align-items-center flex-wrap me-3 mb-5 mb-lg-0">
                                 <!--begin::Title-->
-                                <h1 class="d-flex align-items-center text-dark fw-bolder fs-3 my-1"><?= $pagina_title ?? '' ?></h1>
+                                <h1 class="d-flex align-items-center text-dark fw-bolder fs-3 my-1">
+                                    <?= $pagina_title ?? '' ?></h1>
                                 <!--end::Title-->
                             </div>
                             <!--end::Page title-->
@@ -188,7 +216,8 @@
                 <!--begin::Footer-->
                 <div class="footer py-4 d-flex flex-lg-column" id="kt_footer">
                     <!--begin::Container-->
-                    <div class="container-fluid d-flex flex-column flex-md-row align-items-center justify-content-between">
+                    <div
+                        class="container-fluid d-flex flex-column flex-md-row align-items-center justify-content-between">
                         <!--begin::Copyright-->
                         <div class="text-dark order-2 order-md-1">
                             <!-- <span class="text-muted fw-bold me-1">2021©</span>
@@ -225,8 +254,11 @@
         <!--begin::Svg Icon | path: icons/duotune/arrows/arr066.svg-->
         <span class="svg-icon">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                <rect opacity="0.5" x="13" y="6" width="13" height="2" rx="1" transform="rotate(90 13 6)" fill="black" />
-                <path d="M12.5657 8.56569L16.75 12.75C17.1642 13.1642 17.8358 13.1642 18.25 12.75C18.6642 12.3358 18.6642 11.6642 18.25 11.25L12.7071 5.70711C12.3166 5.31658 11.6834 5.31658 11.2929 5.70711L5.75 11.25C5.33579 11.6642 5.33579 12.3358 5.75 12.75C6.16421 13.1642 6.83579 13.1642 7.25 12.75L11.4343 8.56569C11.7467 8.25327 12.2533 8.25327 12.5657 8.56569Z" fill="black" />
+                <rect opacity="0.5" x="13" y="6" width="13" height="2" rx="1" transform="rotate(90 13 6)"
+                    fill="black" />
+                <path
+                    d="M12.5657 8.56569L16.75 12.75C17.1642 13.1642 17.8358 13.1642 18.25 12.75C18.6642 12.3358 18.6642 11.6642 18.25 11.25L12.7071 5.70711C12.3166 5.31658 11.6834 5.31658 11.2929 5.70711L5.75 11.25C5.33579 11.6642 5.33579 12.3358 5.75 12.75C6.16421 13.1642 6.83579 13.1642 7.25 12.75L11.4343 8.56569C11.7467 8.25327 12.2533 8.25327 12.5657 8.56569Z"
+                    fill="black" />
             </svg>
         </span>
         <!--end::Svg Icon-->
